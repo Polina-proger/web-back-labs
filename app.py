1c1f12e1626131869f8e595ee08cba80beaf1fcc
Полина Селихова <pselihova3@gmail.com>
2025-10-11 10:45:12 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import datetime
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html>
         <html>
@@ -15,7 +15,7 @@ def web():
             'Content-Type': 'text/plain: charset=utf-8'
         }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Селихова Полина Сергеевна"
     group = "ФБИ-33"
@@ -31,7 +31,7 @@ def author():
             </body>
         </html>"""
 
-@app.route("/image")
+@app.route("/lab1/image")
 def image():
     css_url = url_for('static', filename='lab1.css')
     img_url = url_for('static', filename='oak.jpg')
@@ -51,7 +51,7 @@ def image():
 
 count = 0
 
-@app.route('/counter')
+@app.route('/lab1/counter')
 def counter():
     global count
     count += 1
@@ -79,9 +79,9 @@ def clear_counter():
     count = 0
     return redirect('/counter')
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route("/lab1/created")
 def created():
