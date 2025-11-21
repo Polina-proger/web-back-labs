5ee6ac247d540f91d593620144d4bcf76edff1d8
Полина Селихова <pselihova3@gmail.com>
2025-11-22 00:31:28 +0700

    Рефакторинг: выделение первой лабораторной

---
+++ lab1.py
@@ -0,0 +1,292 @@
+from flask import Blueprint, url_for, redirect, request
+lab1 = Blueprint('lab1',__name__)
+import datetime
+
+
+@lab1.route("/index")
+def index():
+    return """<!doctype html>
+<html>
+<head>
+    <title>НГТУ, ФБ, Лабораторные работы</title>
+</head>
+<body>
+    <header>
+        <h1>НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных</h1>
+    </header>
+    
+    <main>
+        <ul>
+            <li><a href="/lab1">Первая лабораторная</a></li>
+            <li><a href="/lab2/">Вторая лабораторная</a></li>
+        </ul>
+    </main>
+    
+    <footer>
+        <hr>
+        <p>Селихова Полина Сергеевна, ФБИ-33, 3 курс, 2025</p>
+    </footer>
+</body>
+</html>"""
+
+
+@lab1.route("/lab1")
+def lab():
+    return """
+<!doctype html>
+<html>
+<head>
+    <title>Лабораторная 1</title>
+</head>
+<body>
+    <h1>Лабораторная работа 1</h1>
+    <p>Flask — фреймворк для создания веб-приложений на языке
+программирования Python, использующий набор инструментов
+Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+называемых микрофреймворков — минималистичных каркасов
+веб-приложений, сознательно предоставляющих лишь самые ба-
+зовые возможности.</p>
+    
+    <ul>
+        <li><a href="/lab1/web">web</a></li>
+        <li><a href="/lab1/author">author</a></li>
+        <li><a href="/lab1/image">image</a></li>
+        <li><a href="/lab1/counter">counter</a></li>
+        <li><a href="/lab1/info">info</a></li>
+        <li><a href="/lab1/created">created</a></li>
+        <li><a href="/lab1/error">error (вызвать ошибку 500)</a></li>
+    </ul>
+
+    <h2>Коды ответов HTTP:</h2>
+    <ul>
+        <li><a href="/lab1/400">400 Bad Request</a></li>
+        <li><a href="/lab1/401">401 Unauthorized</a></li>
+        <li><a href="/lab1/402">402 Payment Required</a></li>
+        <li><a href="/lab1/403">403 Forbidden</a></li>
+        <li><a href="/lab1/405">405 Method Not Allowed</a></li>
+        <li><a href="/lab1/418">418 I'm a teapot</a></li>
+    </ul>
+
+    <h2>Список роутов</h2>
+    <ul>
+        <li><a href="/">/</a></li>
+        <li><a href="/index">/index</a></li>
+        <li><a href="/lab1">/lab1</a></li>
+        <li><a href="/lab1/web">/lab1/web</a></li>
+        <li><a href="/lab1/author">/lab1/author</a></li>
+        <li><a href="/lab1/image">/lab1/image</a></li>
+        <li><a href="/lab1/counter">/lab1/counter</a></li>
+        <li><a href="/lab1/clear_counter">/lab1/clear_counter</a></li>
+        <li><a href="/lab1/info">/lab1/info</a></li>
+        <li><a href="/lab1/created">/lab1/created</a></li>
+        <li><a href="/lab1/error">/lab1/error</a></li>
+        <li><a href="/lab1/400">/lab1/400</a></li>
+        <li><a href="/lab1/401">/lab1/401</a></li>
+        <li><a href="/lab1/402">/lab1/402</a></li>
+        <li><a href="/lab1/403">/lab1/403</a></li>
+        <li><a href="/lab1/405">/lab1/405</a></li>
+        <li><a href="/lab1/418">/lab1/418</a></li>
+    </ul>
+    
+    <a href="/">Назад</a>
+</body>
+</html>"""
+
+
+@lab1.route("/lab1/error")
+def server_error():
+    # Вызываем ошибку деления на ноль
+    result = 10 / 0
+    return "Эта строка никогда не выполнится"
+
+
+@lab1.route("/lab1/400")
+def bad_request():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>400 Bad Request</h1>
+        <p>Сервер не может обработать запрос из-за клиентской ошибки (неправильный синтаксис, неверный формат и т.д.)</p>
+        <a href="/lab1">Назад</a>
+    </body>
+</html>
+''', 400
+
+
+@lab1.route("/lab1/401")
+def unauthorized():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>401 Unauthorized</h1>
+        <p>Требуется аутентификация для доступа к ресурсу</p>
+        <a href="/lab1">Назад</a>
+    </body>
+</html>
+''', 401
+
+
+@lab1.route("/lab1/402")
+def payment_required():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>402 Payment Required</h1>
+        <p>Требуется оплата для доступа к ресурсу (зарезервировано для будущего использования)</p>
+        <a href="/lab1">Назад</a>
+    </body>
+</html>
+''', 402
+
+
+@lab1.route("/lab1/403")
+def forbidden():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>403 Forbidden</h1>
+        <p>Доступ к запрошенному ресурсу запрещен</p>
+        <a href="/lab1">Назад</a>
+    </body>
+</html>
+''', 403
+
+
+@lab1.route("/lab1/405")
+def method_not_allowed():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>405 Method Not Allowed</h1>
+        <p>Метод запроса не поддерживается для данного ресурса</p>
+        <a href="/lab1">Назад</a>
+    </body>
+</html>
+''', 405
+
+
+@lab1.route("/lab1/418")
+def teapot():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>418 I'm a teapot</h1>
+        <p>Я - чайник (шутливый код из RFC 2324)</p>
+        <a href="/lab1">Назад</a>
+    </body>
+</html>
+''', 418
+
+
+@lab1.route("/lab1/web")
+def web():
+    return """<!doctype html>
+        <html>
+          <body>
+             <h1>web-сервер на flask</h1>
+             <a href="/lab1">Назад</a>
+         </body>
+        </html>""", 200, {
+            'X-Server': 'sample',
+            'Content-Type': 'text/plain; charset=utf-8'
+        }
+
+
+@lab1.route("/lab1/author")
+def author():
+    name = "Селихова Полина Сергеевна"
+    group = "ФБИ-33"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/lab1">Назад</a>
+            </body>
+        </html>"""
+
+
+@lab1.route("/lab1/image")
+def image():
+    css_url = url_for('static', filename='lab1.css')
+    img_url = url_for('static', filename='oak.jpg')
+    return f'''
+<!doctype html>
+<html>
+    <head>
+        <title>Стилизованная картинка</title>
+        <link rel="stylesheet" href="{css_url}">
+    </head>
+    <body>
+        <h1>Стилизованный дуб</h1>
+        <img src="{img_url}" alt="Дуб" class="styled-image">
+        <br>
+        <a href="/lab1">Назад</a>
+    </body>
+</html> 
+''', 200, {
+    'Content-Language': 'ru',
+    'X-Student-Name': 'Selikhova Polina',
+    'X-Lab-Number': '1',
+    'X-Custom-Header': 'Flask-Image-Processor'
+}
+
+count = 0
+
+@lab1.route('/lab1/counter')
+def counter():
+    global count
+    count += 1
+    time = datetime.datetime.today()
+    url = request.url
+    client_ip = request.remote_addr
+
+    return '''
+<!doctype html>
+<html>
+    <body>
+        Сколько раз вы сюда заходили: ''' + str(count) + '''
+        <hr>
+        Дата и время: ''' + str(time) + '''<br>
+        Запрошенный адрес: ''' + str(url) + '''<br>
+        Ваш IP адрес: ''' + str(client_ip) + '''<br>
+        <a href="/lab1/clear_counter">Очистить счётчик</a><br>
+        <a href="/lab1">Назад</a>
+    </body>
+</html> 
+'''
+
+
+@lab1.route('/lab1/clear_counter')
+def clear_counter():
+    global count
+    count = 0
+    return redirect('/lab1/counter')
+
+
+@lab1.route("/lab1/info")
+def info():
+    return redirect("/lab1/author")
+
+
+@lab1.route("/lab1/created")
+def created():
+    return '''
+<!doctype html>
+<html>
+    <body>
+        <h1>Создано успешно</h1>
+        <div><i>что-то создано...</i></div>
+        <a href="/lab1">Назад</a>
+    </body>
+</html>     
+''', 201
