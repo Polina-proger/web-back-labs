b41b99bee09ed5d606de31e1b1be4a40e8be5821
Полина Селихова <pselihova3@gmail.com>
2025-12-18 22:10:25 +0700

    Счётчик деревьев

--- lab4.py
+++ lab4.py
@@ -31,3 +31,88 @@ def div():
     
     except ValueError:
         return render_template('lab4/div.html', error='Ошибка: введите числа!')
+
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('lab4/sum-form.html')
+
+@lab4.route('/lab4/sum', methods=['POST'])
+def sum():
+    x1 = request.form.get('x1', '0')
+    x2 = request.form.get('x2', '0')
+    
+    x1 = int(x1) if x1 != '' else 0
+    x2 = int(x2) if x2 != '' else 0
+    
+    result = x1 + x2
+    return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/mul-form')
+def mul_form():
+    return render_template('lab4/mul-form.html')
+
+@lab4.route('/lab4/mul', methods=['POST'])
+def mul():
+    x1 = request.form.get('x1', '1')
+    x2 = request.form.get('x2', '1') 
+    
+    x1 = int(x1) if x1 != '' else 1
+    x2 = int(x2) if x2 != '' else 1
+    
+    result = x1 * x2
+    return render_template('lab4/mul.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('lab4/sub-form.html')
+
+@lab4.route('/lab4/sub', methods=['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', x1=x1, x2=x2, error='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    
+    result = x1 - x2
+    return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
+
+@lab4.route('/lab4/pow-form')
+def pow_form():
+    return render_template('lab4/pow-form.html')
+
+@lab4.route('/lab4/pow', methods=['POST'])
+def power():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    
+    if x1 == '' or x2 == '':
+        return render_template('lab4/pow.html', x1=x1, x2=x2, error='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+    
+    if x1 == 0 and x2 == 0:
+        return render_template('lab4/pow.html', x1=x1, x2=x2, error='Ноль в нулевой степени не определен!')
+    
+    result = x1 ** x2
+    return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
+
+tree_count = 0
+
+@lab4.route('/lab4/tree', methods = ['GET', 'POST'])
+def tree():
+    global tree_count
+    if request.method == 'GET':
+        return render_template ('lab4/tree.html', tree_count=tree_count)
+    
+    operation = request.form.get('operation')
+    if operation ==' cut':
+        tree_count -= 1
+    elif operation == 'plant':
+        tree_count += 1
+
+    return render_template('lab4/tree.html', tree_count = tree_count)
